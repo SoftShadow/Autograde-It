e5a9a936c908becdd86bfac57d07ba83e9f6b6eb
Henry Bradlow <hbradlow@berkeley.edu>
2012-11-06 10:47:26 -0800

    Added view for api key

--- autograde_it/autograde_it/apps/example_project/urls.py
+++ autograde_it/autograde_it/apps/example_project/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('example_project.views',
     url(r'^feedback/',"user_feedback",name="feedback"),
+    url(r'^api_key/',"api_key",name="api_key"),
 )
--- autograde_it/autograde_it/apps/example_project/views.py
+++ autograde_it/autograde_it/apps/example_project/views.py
@@ -15,3 +15,9 @@ from example_project.forms import UserFeedbackForm
 def user_feedback(request):
     form = UserFeedbackForm()
     return render_to_response("user_feedback.html",{"form":form},context_instance=RequestContext(request))
+
+@login_required
+def api_key(request):
+    print request.user
+    api_key = request.user.api_key
+    return render_to_response("api_key.html",{"api_key":api_key},context_instance=RequestContext(request))
